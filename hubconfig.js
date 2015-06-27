9c2eeb22aa93e22e3720617fcb90272ec844747f
cinada <akkun1983@gmail.com>
2015-06-27 02:19:51 +0000

    hubconfig.js created online with Bitbucket

---
+++ hubconfig.js
@@ -0,0 +1,20 @@
+global.DEFAULT_HTTP_PORT = 80;
+
+function Hosts() {
+    var self = this;
+    this.routes;
+}
+var h = new Hosts();
+
+setTimeout(function(){
+    h.routes = {
+        "DEFAULT":{host:"localhost",port:80},
+        "8082":{host:"localhost",port:8082},
+        "8083":{host:"localhost",port:8083},
+        "3012":{host:"ec2-23-23-199-244.compute-1.amazonaws.com",port:3012},
+        "esri":{host:"esri.com",port:80},
+        "google":{host:"google.com",port:80}
+    };
+
+},5000);
+module.exports = h;
