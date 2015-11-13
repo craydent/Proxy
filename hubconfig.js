e353baf0312c64c95e632f8ad66052390614db6c
cinada <akkun1983@gmail.com>
2015-11-13 11:52:17 -0800

    adding comments and some documentation

--- hubconfig.js
+++ hubconfig.js
@@ -1,29 +1,27 @@
 global.DEFAULT_HTTP_PORT = 80;
 
 function Hosts() {
+    /*|{
+
+    }|*/
     var self = this;
     self.routes = {
         "DEFAULT":{host:"localhost",port:8080},
-        "3010":{host:"localhost",port:3010},
-        "3012":{host:"localhost",port:3012},
-        "socket":{host:"localhost",port:3010},
-        "rest":{host:"localhost",port:3012},
+        "route_path":{host:"",port:80}
+        //"Key":{host:"(Array or String) host or domain we should forward to",port:3010/*Integer or Array of ports*/,path:"",headers:{}}
+        /*
+            Assume route is {"routeOne":{"host":"www.google.com",port:80,path:"/myRoute",headers:{"Header Name":"(String or Function) Overwrite Value"}}}
+                and this proxy is running on port 80 on www.example.com
+            Key is the route name or route path.
+            EX: if a request comes through on http://www.example.com/routeOne/subdirectory/file.html
+                the request will be forwarded to http://www.google.com/myRoute/subdirectory/file.html
+            if there is a header in the request called "Header Name" (regardless of the previous Value)
+                will forward the request with the header defined in the config
+        */
     };
     self.port = 80;
     self.host = "localhost";
 }
 var h = new Hosts();
-/*
-setTimeout(function(){
-    h.routes = {
-        "DEFAULT":{host:"localhost",port:80},
-        "8082":{host:"localhost",port:8082},
-        "8083":{host:"localhost",port:8083},
-        "3000":{host:"localhost",port:[3000,3001]},
-        "3012":{host:"ec2-23-23-199-244.compute-1.amazonaws.com",port:3012},
-        "esri":{host:"esri.com",port:80},
-        "google":{host:"google.com",port:80}
-    };
 
-},5000);*/
 module.exports = h;
